8e7e367ddee314a28525b49f8e30c1159042dc3e
williford abunas <abunaswilliford.com>
2024-01-16 04:00:35 +1300

    added scripts in package.json and vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/williford-abunas.github.io/williford-abunas.github.io',
   plugins: [react()],
+  base: '/deploy_react_app_github_pages_vercel',
 })
